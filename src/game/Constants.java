23de02a52f63e562223b2b1b3712180ee770d4a6
samuelvalverdee <savalverde@estudiantec.cr>
2023-03-08 14:29:07 -0600

    Inicializacion de clases base

---
+++ src/game/Constants.java
@@ -0,0 +1,19 @@
+package game;
+
+public interface Constants {
+
+	public static final int MAX_ENERGIA = 150;
+	public static final int MIN_ENERGIA = 75;
+	
+	public static final int MAX_VISION = 150;
+	public static final int MIN_VISION = 75;
+	
+	public static final int MAX_VELOCIDAD = 100;
+	public static final int MIN_VELOCIDAD = 25;
+	
+	public static final int MAX_EDAD = 40;
+	public static final int MIN_EDAD = 10;
+	
+	// Agregar constantes de efectos entre atributos
+	
+}
---
+++ src/game/Microorganismo.java
@@ -0,0 +1,21 @@
+package game;
+
+public class Microorganismo {
+
+	public int energia;
+	public int vision;
+	public int velocidad;
+	public int edad;
+	
+	// Clase Padre de Microorganismos
+	
+	public Microorganismo(int pEnergia, int pVision, int pVelocidad, int pEdad) {
+		
+		this.energia = pEnergia;
+		this.vision = pVision;
+		this.velocidad = pVelocidad;
+		this.edad = pEdad;
+		
+	}
+	
+}
