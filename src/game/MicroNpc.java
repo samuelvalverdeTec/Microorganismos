dc8b23797a33ed82049e3c949be237523063132f
Erick25Kauffmann <erikaupor@gmail.com>
2023-03-23 19:26:39 -0600

    Constructor MicroNPC

--- src/game/MicroNpc.java
+++ src/game/MicroNpc.java
@@ -4,6 +4,22 @@ public class MicroNpc extends Microorganismo {
 
 	public MicroNpc() {
 		
+		int pEnergia = (int)(Math.random()*MAX_ENERGIA+MIN_ENERGIA);
+		int pVision = (int)(Math.random()*MAX_VISION+MIN_VISION);
+		int pVelocidad = (int)(Math.random()*MAX_VELOCIDAD+MIN_VELOCIDAD);
+		int pEdad = (int)(Math.random()*MAX_EDAD+MIN_EDAD);
+		
+		int pPosX = (int)(Math.random()*TABLERO_SIZE_1+1);
+		int pPosY = (int)(Math.random()*TABLERO_SIZE_2+1);
+		
+		this.energia = pEnergia;
+		this.vision = pVision;
+		this.velocidad = pVelocidad;
+		this.edad = pEdad;
+		
+		this.posX = pPosX;
+		this.posY = pPosY;
+		
 	}
 	
 }
