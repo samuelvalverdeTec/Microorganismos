61492d92ed4d6db62266597dff05761304e63700
Erick25Kauffmann <erikaupor@gmail.com>
2023-03-24 19:17:42 -0600

    Key Listener

--- src/game/MyMicro.java
+++ src/game/MyMicro.java
@@ -1,6 +1,10 @@
 package game;
 
-public class MyMicro extends Microorganismo {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+
+public class MyMicro extends Microorganismo implements KeyListener {
 
 	public MyMicro() {
 		
@@ -23,6 +27,79 @@ public class MyMicro extends Microorganismo {
 		this.energia += alimento.energiaAl;
 		
 	}
+	
+	
+	
+	
+	@Override
+	public void keyTyped(KeyEvent e) {
+	}
+
+
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if(this != null) {
+			int key = e.getKeyCode();
+		    if (key == KeyEvent.VK_LEFT) {
+		    	if(this.isAlive) {
+			    	if(this.energia > 0) {
+			    		this.posX = this.posX - VELOCIDAD_JUGADOR;
+			    	}
+		    	}
+		    }
+	
+		    else if (key == KeyEvent.VK_RIGHT) {
+		    	if(this.isAlive) {
+		    		if(this.energia > 0) {
+			    		this.posX = this.posX + VELOCIDAD_JUGADOR;
+			    	}
+		    	}
+		    }
+	
+		    else if (key == KeyEvent.VK_UP) {
+		    	if(this.isAlive) {
+		    		if(this.energia > 0) {
+			    		this.posY = this.posY + VELOCIDAD_JUGADOR;
+			    	}
+		    	}
+		    }
+	
+		    else if (key == KeyEvent.VK_DOWN) {
+		    	if(this.isAlive) {
+		    		if(this.energia > 0) {
+			    		this.posY = this.posY - VELOCIDAD_JUGADOR;
+			    	}
+		    	}    	
+		    }
+
+		}
+	}
+
+
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		/*if(this != null) {
+			int key = e.getKeyCode();
+		    if (key == KeyEvent.VK_LEFT) {
+		    	
+		    }
+	
+		    else if (key == KeyEvent.VK_RIGHT) {
+		    	
+		    }
+	
+		    else if (key == KeyEvent.VK_UP) {
+		    	
+		    }
+	
+		    else if (key == KeyEvent.VK_DOWN) {
+		    	
+		    }
+		}*/
+	}
+	
 	
 	
 }
