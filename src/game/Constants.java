2a78040035fb31c747003e9a9384cbc280b0559f
samuelvalverdee <savalverde@estudiantec.cr>
2023-04-01 14:03:30 -0600

    GUI - creacion y refrescar()

--- src/game/Constants.java
+++ src/game/Constants.java
@@ -14,18 +14,16 @@ public interface Constants {
 	public static final int MAX_EDAD = 100;
 	public static final int MIN_EDAD = 1;
 	
-	public static final int TABLERO_SIZE_1 = 10;
-	public static final int TABLERO_SIZE_2 = 10;
+	public static final int TABLERO_SIZE_1 = 20;
+	public static final int TABLERO_SIZE_2 = 20;
 	
-	public static final int CANTIDAD_NPCS = 249;
+	public static final int CANTIDAD_NPCS = (TABLERO_SIZE_1*TABLERO_SIZE_2)/10;
 	
 	
 	public static final int EDAD_JUGADOR = 20;
 	public static final int ENERGIA_JUGADOR = 100;
 	public static final int VISION_JUGADOR = 3;
 	public static final int VELOCIDAD_JUGADOR = 2;
-	
-	
 	
 	// Agregar constantes de efectos entre atributos
 	public static final int DISMINUCION_ENERGIA_POR_CASILLA = 5;
--- src/gui/Interfaz.java
+++ src/gui/Interfaz.java
@@ -1,33 +1,80 @@
 package gui;
 
+import java.awt.Color;
 import java.awt.GridLayout;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import game.Constants;
+import game.Mapeable;
 
 public class Interfaz implements Constants {
 
+	JFrame frame;
+	JPanel panel;
+	JButton gui[][];
+	
+	public static String rutaImagenes = "C:\\Users\\jcval\\OneDrive - Estudiantes ITCR\\IS2023\\poo\\Microorganismos\\src\\";
+	public String rutaA = "alimento.jpg";
+	public String rutaM = "micro.jpg";
+	public BufferedImage imgA = null;
+	public BufferedImage imgM = null;
+	
 	public Interfaz() {
 	
-		int mapa[][] = new int[TABLERO_SIZE_1][TABLERO_SIZE_2];
+		this.gui = new JButton [TABLERO_SIZE_1][TABLERO_SIZE_2];
 		
-		JFrame f = new JFrame("Microorganismos");
-	    JPanel p = new JPanel();
-	    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
-	    p.setLayout(new GridLayout(TABLERO_SIZE_1, TABLERO_SIZE_2));
+		this.frame = new JFrame("Microorganismos");
+	    this.panel = new JPanel();
+	    
+	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+	    panel.setLayout(new GridLayout(TABLERO_SIZE_1, TABLERO_SIZE_2));
 
-	    for(int r = 0; r < TABLERO_SIZE_1; r++){
+	    for(int f = 0; f < TABLERO_SIZE_1; f++){
 	        for(int c = 0; c < TABLERO_SIZE_2; c++){
-	            ChangingButton button= new ChangingButton(r, c, mapa);
-	            p.add(button);
+	            JButton casilla = new JButton();
+	            gui[f][c] = casilla;
+	            panel.add(casilla);
 	        }
 	    }
-	    f.add(p);
-	    f.pack();
-	    f.setVisible(true);
+	    
+		try {
+			
+			this.imgA = ImageIO.read(new File(rutaImagenes + rutaA));
+			this.imgM = ImageIO.read(new File(rutaImagenes + rutaM));
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	    
+	    frame.add(panel);
+	    frame.pack();
+	    frame.setVisible(true);
 		
 	}
 
+	public void refrescar(Mapeable[][] mapa) {
+		
+		for(int f = 0; f < TABLERO_SIZE_1; f++){
+	        for(int c = 0; c < TABLERO_SIZE_2; c++){
+	        	JButton casilla = gui[f][c];
+	            if(mapa[f][c].esAlimento()) {
+	            	casilla.setIcon(new ImageIcon(this.imgA));	//alimento
+	            } else if(mapa[f][c].esAlimento() == false) {
+	            	casilla.setIcon(new ImageIcon(this.imgM));	//micro
+	            } else {
+	            }
+	            casilla.setBackground(Color.lightGray);
+	        }
+	    }
+		
+	}
+	
 }
